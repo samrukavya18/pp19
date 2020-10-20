390721b56193ac8be124e7b32c1d90bc11a58946
samrukavya18 <samrukavya18@gmail.com>
2020-10-20 18:18:43 +0530

    Employee Daily Wage

--- jemp.java
+++ jemp.java
@@ -1,11 +1,14 @@
 public class jemp {
    public static void main(String[] args){
       int IS_FULL_TIME =1;
+      int EMP_RATE_PER_HOUR = 20;
 
+       int empHrs = 0;
+       int empWage = 0;
       double empCheck = Math.floor(Math.random() * 10) % 2;
        if (empCheck == IS_FULL_TIME)
-           System.out.println("Employee is  Present");
-       else
-           System.out.println("Employee  is Absent");
+           empHrs = 8;
+         empWage = empHrs  * EMP_RATE_PER_HOUR;
+          System.out.println("Emp Wage : " + empWage);
      }
 }

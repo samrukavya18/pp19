3dd7a43ee0d4426f21d3a593307910d28f80eddf
samrukavya18 <samrukavya18@gmail.com>
2020-10-22 17:21:58 +0530

    20  Working Days

--- jemp.java
+++ jemp.java
@@ -3,6 +3,7 @@ public class jemp {
      public static final int IS_PART_TIME = 1;
       public static final int IS_FULL_TIME = 2;
       public static final int EMP_RATE_PER_HOUR = 20;
+      public static final int NUM_OF_WORKING_DAYS = 2;
 
 
 
@@ -12,7 +13,9 @@ public class jemp {
 
        int empHrs = 0;
        int empWage = 0;
+       int totalEmpWage = 0;
 
+for (int day = 0; day < NUM_OF_WORKING_DAYS; day++) {
       int empCheck = (int)Math.floor(Math.random() * 10) % 3;
 
        switch (empCheck) {
@@ -27,7 +30,10 @@ public class jemp {
          }
 
       empWage = empHrs  * EMP_RATE_PER_HOUR;
+       totalEmpWage += empWage;
        System.out.println( "Emp Wage : " + empWage);
 
      }
+      System.out.println("Total Emp Wage: " + totalEmpWage);
+   }
 }
